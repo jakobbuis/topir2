bb3385f10ce7280128ca28a1facdd65f6dc20de1
Jakob Buis <jakob@jakobbuis.nl>
2020-05-03 16:08:49 +0200

    subsitute Todoist login for basic auth

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,18 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
+use Illuminate\Support\Facades\Session;
 
-class Authenticate extends Middleware
+class Authenticate
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
+    public function handle($request, Closure $next)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (Session::has('current_user')) {
+            return $next($request);
         }
+
+        return redirect(route('login'));
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 // OAuth2 Todoist
-Route::get('login/todoist', 'Auth\LoginController@redirectToProvider');
+Route::get('login/todoist', 'Auth\LoginController@redirectToProvider')->name('login');
 Route::get('login/todoist/callback', 'Auth\LoginController@handleProviderCallback');
 
-Route::middleware('auth.basic')->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/', 'DashboardController');
 });
