7849a0d2fb5e0ad95989b56012a68af47c4e7613
Jakob Buis <jakob@jakobbuis.nl>
2020-05-03 15:05:45 +0200

    disable superfluous Mix success notifications

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,3 +2,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js').version();
 mix.sass('resources/sass/app.scss', 'public/css').version();
+
+mix.disableSuccessNotifications();
