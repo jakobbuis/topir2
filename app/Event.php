883361bc9d642437bb68e2662755139f3387b714
Jakob Buis <jakob@jakobbuis.nl>
2021-04-07 22:10:18 +0200

    switch to class-based factories

--- app/Event.php
+++ app/Event.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use App\Events\EventCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    use HasFactory;
+
     protected $casts = [
         'data' => 'object',
     ];
