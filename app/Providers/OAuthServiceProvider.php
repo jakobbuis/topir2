607da017f2e08be1b165e7480e90421f49e69a76
Jakob Buis <jakob@jakobbuis.nl>
2021-11-20 18:50:09 +0100

    pass larastan level 2 with some ignores for Socialite

--- app/Providers/OAuthServiceProvider.php
+++ app/Providers/OAuthServiceProvider.php
@@ -12,9 +12,11 @@ class OAuthServiceProvider extends ServiceProvider
     {
         // Add the custom Todoist driver to Socialite
         $socialite = app(Factory::class);
+        // @phpstan-ignore-next-line
         $socialite->extend(
             'todoist',
             function ($app) use ($socialite) {
+                // @phpstan-ignore-next-line
                 return $socialite->buildProvider(TodoistSocialiteProvider::class, config('services.todoist'));
             }
         );
